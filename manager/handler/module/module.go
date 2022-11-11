cab8cb7833ec3a73bb50f1af1ff4f04123cc11a8
y-du <42994541+y-du@users.noreply.github.com>
2022-11-11 16:18:02 +0100

    init

---
+++ manager/handler/module/module.go
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2022 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package module
+
+import (
+	"errors"
+	"fmt"
+	"github.com/SENERGY-Platform/go-service-base/srv-base"
+	"github.com/SENERGY-Platform/go-service-base/srv-base/types"
+	"gopkg.in/yaml.v3"
+	"module-manager/manager/itf"
+	"module-manager/manager/itf/modfile"
+	"module-manager/manager/itf/module"
+	"net/http"
+	"os"
+)
+
+type Handler struct {
+	storageHandler itf.ModuleStorageHandler
+}
+
+func NewHandler(storageHandler itf.ModuleStorageHandler) *Handler {
+	return &Handler{storageHandler: storageHandler}
+}
+
+func (h *Handler) List() ([]module.Module, error) {
+	var modules []module.Module
+	mIds, err := h.storageHandler.List()
+	if err != nil {
+		return modules, srv_base_types.NewError(http.StatusInternalServerError, "listing modules failed", err)
+	}
+	for _, id := range mIds {
+		modFile, err := h.storageHandler.Open(id)
+		if err != nil {
+			srv_base.Logger.Errorf("opening module '%s' failed: %s", id, err)
+			continue
+		}
+		var m module.Module
+		yd := yaml.NewDecoder(modFile)
+		var mf modfile.ModFile
+		err = yd.Decode(&mf)
+		if err != nil {
+			srv_base.Logger.Errorf("decoding modfile '%s' failed: %s", id, err)
+			continue
+		}
+		m, err = mf.ParseModule()
+		if err != nil {
+			srv_base.Logger.Errorf("parsing module '%s' failed: %s", id, err)
+			continue
+		}
+		modules = append(modules, m)
+	}
+	return modules, nil
+}
+
+func (h *Handler) Read(id string) (module.Module, error) {
+	var m module.Module
+	modFile, err := h.storageHandler.Open(id)
+	if err != nil {
+		code := http.StatusInternalServerError
+		if os.IsNotExist(errors.Unwrap(err)) {
+			code = http.StatusNotFound
+		}
+		return m, srv_base_types.NewError(code, fmt.Sprintf("opening module '%s' failed", id), err)
+	}
+	yd := yaml.NewDecoder(modFile)
+	var mf modfile.ModFile
+	err = yd.Decode(&mf)
+	if err != nil {
+		return m, srv_base_types.NewError(http.StatusInternalServerError, fmt.Sprintf("decoding modfile '%s' failed", id), err)
+	}
+	m, err = mf.ParseModule()
+	if err != nil {
+		return m, srv_base_types.NewError(http.StatusInternalServerError, fmt.Sprintf("parsing module '%s' failed", id), err)
+	}
+	return m, nil
+}
+
+func (h *Handler) Add(id string) error {
+	return nil
+}
+
+func (h *Handler) Delete(id string) error {
+	if err := h.storageHandler.Delete(id); err != nil {
+		code := http.StatusInternalServerError
+		if os.IsNotExist(errors.Unwrap(err)) {
+			code = http.StatusNotFound
+		}
+		return srv_base_types.NewError(code, fmt.Sprintf("deleting module '%s' failed", id), err)
+	}
+	return nil
+}
+
+func (h *Handler) Update(id string) error {
+	return nil
+}
