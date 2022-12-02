c9c0d57a34c203ec5ab5b4dbda95aaf9628c2dd8
y-du <42994541+y-du@users.noreply.github.com>
2022-12-02 15:45:24 +0100

    init

---
+++ manager/handler/config_def/loader.go
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2022 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package config_def
+
+import (
+	"encoding/json"
+	"module-manager/manager/itf"
+	"os"
+)
+
+func Load(path string) (map[string]itf.ConfigDefinition, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	decoder := json.NewDecoder(file)
+	var d map[string]itf.ConfigDefinition
+	if err = decoder.Decode(&d); err != nil {
+		return nil, err
+	}
+	return d, nil
+}
