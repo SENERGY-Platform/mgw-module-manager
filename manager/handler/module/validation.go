f8c58b14a2643ee77853361eb11569926f4229bb
y-du <42994541+y-du@users.noreply.github.com>
2022-12-05 09:07:48 +0100

    check if services exist

--- manager/handler/module/validation.go
+++ manager/handler/module/validation.go
@@ -50,12 +50,8 @@ func Validate(m itf.Module) error {
 			if err := sem_ver.ValidateSemVerRange(dependency.Version); err != nil {
 				return fmt.Errorf("dependency '%s': %s", mid, err)
 			}
-			if dependency.RequiredServices != nil {
-				for s := range dependency.RequiredServices {
-					if s == "" {
-						return fmt.Errorf("invalid service name for dependency '%s'", mid)
-					}
-				}
+			if dependency.RequiredServices == nil {
+				return fmt.Errorf("missing services for dependency '%s'", mid)
 			}
 		}
 	}
