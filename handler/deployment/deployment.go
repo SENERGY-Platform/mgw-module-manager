fb06236a6e9b23bc784b97083ad8ce0c6a106e96
y-du <42994541+y-du@users.noreply.github.com>
2023-03-27 16:14:09 +0200

    refactor

--- handler/deployment/deployment.go
+++ handler/deployment/deployment.go
@@ -60,41 +60,5 @@ func (h *Handler) Update(id string) error {
 }
 
 func (h *Handler) InputTemplate(m *module.Module) model.InputTemplate {
-	it := model.InputTemplate{
-		HostResources: make(map[string]model.InputTemplateHostRes),
-		Secrets:       make(map[string]model.InputTemplateSecret),
-		Configs:       make(map[string]model.InputTemplateConfig),
-		InputGroups:   m.Inputs.Groups,
-	}
-	for ref, input := range m.Inputs.Resources {
-		it.HostResources[ref] = model.InputTemplateHostRes{
-			Input:        input,
-			HostResource: m.HostResources[ref],
-		}
-	}
-	for ref, input := range m.Inputs.Secrets {
-		it.Secrets[ref] = model.InputTemplateSecret{
-			Input:  input,
-			Secret: m.Secrets[ref],
-		}
-	}
-	for ref, input := range m.Inputs.Configs {
-		cv := m.Configs[ref]
-		itc := model.InputTemplateConfig{
-			Input:    input,
-			Default:  cv.Default,
-			Options:  cv.Options,
-			OptExt:   cv.OptExt,
-			Type:     cv.Type,
-			TypeOpt:  make(map[string]any),
-			DataType: cv.DataType,
-			IsList:   cv.IsSlice,
-			Required: cv.Required,
-		}
-		for key, opt := range cv.TypeOpt {
-			itc.TypeOpt[key] = opt.Value
-		}
-		it.Configs[ref] = itc
-	}
-	return it
+	return genInputTemplate(m)
 }
