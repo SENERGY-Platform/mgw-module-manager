478c46b8a4a4a7ffff6a94ef5ea1c2d9ab666b42
y-du <42994541+y-du@users.noreply.github.com>
2023-04-24 08:44:59 +0200

    add function for getting order of deployments

--- handler/deployment/deployment.go
+++ handler/deployment/deployment.go
@@ -87,3 +87,27 @@ func (h *Handler) getReqDep(ctx context.Context, dep *model.Deployment, reqDep m
 	}
 	return nil
 }
+
+func (h *Handler) getDepOrder(dep map[string]*model.Deployment) (order []string, err error) {
+	if len(dep) > 1 {
+		nodes := make(tsort.Nodes)
+		for _, d := range dep {
+			if len(d.RequiredDep) > 0 {
+				reqIDs := make(map[string]struct{})
+				for _, i := range d.RequiredDep {
+					reqIDs[i] = struct{}{}
+				}
+				nodes.Add(d.ID, reqIDs, nil)
+			}
+		}
+		order, err = tsort.GetTopOrder(nodes)
+		if err != nil {
+			return nil, err
+		}
+	} else if len(dep) > 0 {
+		for _, d := range dep {
+			order = append(order, d.ID)
+		}
+	}
+	return
+}
