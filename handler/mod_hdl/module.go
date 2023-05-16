c5e86808c377160edd6aa862eb15025346a59b83
y-du <42994541+y-du@users.noreply.github.com>
2023-05-16 10:00:11 +0200

    move to separate file

--- handler/mod_hdl/module.go
+++ handler/mod_hdl/module.go
@@ -24,15 +24,12 @@ import (
 	cew_model "github.com/SENERGY-Platform/mgw-container-engine-wrapper/lib/model"
 	"github.com/SENERGY-Platform/mgw-module-lib/module"
 	"github.com/SENERGY-Platform/mgw-module-lib/validation"
-	"github.com/SENERGY-Platform/mgw-module-lib/validation/sem_ver"
 	"github.com/SENERGY-Platform/mgw-module-manager/handler"
 	"github.com/SENERGY-Platform/mgw-module-manager/lib/model"
 	"github.com/SENERGY-Platform/mgw-module-manager/util/context_hdl"
 	"github.com/SENERGY-Platform/mgw-module-manager/util/dir_fs"
 	"io/fs"
 	"net/url"
-	"os"
-	"sort"
 	"strings"
 	"time"
 )
@@ -93,133 +90,6 @@ func (h *Handler) GetIncl(ctx context.Context, mID string) (dir_fs.DirFS, error)
 		}
 	}
 	return dir, nil
-}
-
-func (h *Handler) Add(ctx context.Context, mr model.ModRequest) error {
-	m, err := h.storageHandler.Get(ctx, mr.ID)
-	if err != nil {
-		var nfe *model.NotFoundError
-		if !errors.As(err, &nfe) {
-			return err
-		}
-	}
-	if m.Module != nil {
-		return model.NewInternalError(errors.New("already installed"))
-	}
-	return h.add(ctx, mr.ID, mr.Version, "", false)
-}
-
-func (h *Handler) add(ctx context.Context, mID, ver, verRng string, indirect bool) error {
-	if ver == "" {
-		var err error
-		ver, err = h.getVersion(ctx, mID, verRng)
-		if err != nil {
-			return err
-		}
-	} else {
-		if !sem_ver.IsValidSemVer(ver) {
-			return model.NewInvalidInputError(fmt.Errorf("version '%s' invalid", ver))
-		}
-	}
-	dir, err := h.transferHandler.Get(ctx, mID, ver)
-	if err != nil {
-		return err
-	}
-	defer os.RemoveAll(dir.Path())
-	f, name, err := h.modFileHandler.GetModFile(dir)
-	if err != nil {
-		return err
-	}
-	m, err := h.modFileHandler.GetModule(f)
-	if err != nil {
-		return err
-	}
-	if err = h.validateModule(m, mID, ver); err != nil {
-		return err
-	}
-	if indirect && m.DeploymentType == module.MultipleDeployment {
-		return model.NewInternalError(fmt.Errorf("dependencies with deployment type '%s' not supported", module.MultipleDeployment))
-	}
-	for dmID, dmVerRng := range m.Dependencies {
-		dm, err := h.storageHandler.Get(ctx, dmID)
-		if err != nil {
-			var nfe *model.NotFoundError
-			if !errors.As(err, &nfe) {
-				return err
-			}
-			err = h.add(ctx, dmID, "", dmVerRng, true)
-			if err != nil {
-				return err
-			}
-			continue
-		}
-		if dm.DeploymentType == module.MultipleDeployment {
-			return model.NewInternalError(fmt.Errorf("dependencies with deployment type '%s' not supported", module.MultipleDeployment))
-		}
-		ok, err := sem_ver.InSemVerRange(dmVerRng, dm.Version)
-		if err != nil {
-			return model.NewInternalError(err)
-		}
-		if !ok {
-			return fmt.Errorf("'%s' of '%s' does not satsify '%s'", dm.Version, dm.ID, dmVerRng)
-		}
-	}
-	for _, srv := range m.Services {
-		err = h.addImage(ctx, srv.Image)
-		if err != nil {
-			return err
-		}
-	}
-	err = h.storageHandler.Add(ctx, dir, mID, name, indirect)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func (h *Handler) addImage(ctx context.Context, img string) error {
-	ctxWt, cf := context.WithTimeout(ctx, h.httpTimeout)
-	defer cf()
-	jID, err := h.cewClient.AddImage(ctxWt, img)
-	if err != nil {
-		return model.NewInternalError(err)
-	}
-	job, err := h.cewJobHandler.AwaitJob(ctx, jID)
-	if err != nil {
-		return err
-	}
-	if job.Error != nil {
-		return model.NewInternalError(fmt.Errorf("%v", job.Error))
-	}
-	return nil
-}
-
-func (h *Handler) getVersion(ctx context.Context, mID, verRng string) (string, error) {
-	verList, err := h.transferHandler.ListVersions(ctx, mID)
-	if err != nil {
-		return "", err
-	}
-	sort.Strings(verList)
-	var ver string
-	for i := len(verList) - 1; i >= 0; i-- {
-		v := verList[i]
-		if verRng != "" {
-			ok, _ := sem_ver.InSemVerRange(verRng, v)
-			if ok {
-				ver = v
-				break
-			}
-		} else {
-			if sem_ver.IsValidSemVer(v) {
-				ver = v
-				break
-			}
-		}
-	}
-	if ver == "" {
-		return "", model.NewInternalError(errors.New("no versions available"))
-	}
-	return ver, nil
 }
 
 func (h *Handler) Delete(ctx context.Context, mID string) error {
