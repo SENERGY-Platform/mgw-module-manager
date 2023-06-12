52fe83be63835c78d522533450fb79a6e4405310
y-du <42994541+y-du@users.noreply.github.com>
2023-06-12 14:48:26 +0200

    fix after interface change

--- handler/dep_hdl/stop.go
+++ handler/dep_hdl/stop.go
@@ -85,7 +85,7 @@ func (h *Handler) stop(ctx context.Context, dep *model.Deployment) error {
 	}
 	ctxWt, cf := context.WithTimeout(ctx, h.dbTimeout)
 	defer cf()
-	if err = h.storageHandler.UpdateDep(ctxWt, dep.ID, dep.Name, true, dep.Indirect, time.Now().UTC()); err != nil {
+	if err = h.storageHandler.UpdateDep(ctxWt, dep.ID, dep.Name, dep.Dir, true, dep.Indirect, time.Now().UTC()); err != nil {
 		return err
 	}
 	return nil
