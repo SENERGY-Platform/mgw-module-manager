ba58320d454aab6d336d8aea4ba2d0276bbb990c
y-du <42994541+y-du@users.noreply.github.com>
2023-06-15 15:26:20 +0200

    change update method of module handler interface

--- handler/itf.go
+++ handler/itf.go
@@ -32,8 +32,8 @@ type ModuleHandler interface {
 	GetReq(ctx context.Context, mID string) (model.Module, map[string]model.Module, error)
 	GetIncl(ctx context.Context, mID string) (dir_fs.DirFS, error)
 	Add(ctx context.Context, mod *module.Module, modDir dir_fs.DirFS, modFile string, indirect bool) error
+	Update(ctx context.Context, mod *module.Module, modDir dir_fs.DirFS, modFile string, indirect bool) error
 	Delete(ctx context.Context, mID string, force bool) error
-	Update(ctx context.Context, mID string) error
 }
 
 type ModFileHandler interface {
