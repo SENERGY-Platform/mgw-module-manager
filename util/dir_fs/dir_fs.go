f453109914c1dd0a00f163a6ec0ac7130210b9b9
y-du <42994541+y-du@users.noreply.github.com>
2023-06-19 18:15:28 +0200

    add copy function

--- util/dir_fs/dir_fs.go
+++ util/dir_fs/dir_fs.go
@@ -17,6 +17,8 @@
 package dir_fs
 
 import (
+	"errors"
+	"io"
 	"io/fs"
 	"os"
 )
@@ -65,4 +67,71 @@ func (d DirFS) Sub(name string) (DirFS, error) {
 
 func (d DirFS) Path() string {
 	return string(d)
+}
+
+func Copy(src DirFS, dst string) error {
+	srcStat, err := src.Stat(".")
+	if err != nil {
+		return err
+	}
+	dirEntries, err := fs.ReadDir(src, ".")
+	if err != nil {
+		return err
+	}
+	err = os.MkdirAll(dst, srcStat.Mode())
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if err != nil {
+			os.RemoveAll(dst)
+		}
+	}()
+	for _, entry := range dirEntries {
+		var entryInfo fs.FileInfo
+		entryInfo, err = entry.Info()
+		if err != nil {
+			return err
+		}
+		dstEntryPath := dst + "/" + entryInfo.Name()
+		if entryInfo.IsDir() {
+			err = os.MkdirAll(dstEntryPath, entryInfo.Mode())
+			if err != nil {
+				return err
+			}
+			var srcSubDir DirFS
+			srcSubDir, err = src.Sub(entryInfo.Name())
+			if err != nil {
+				return err
+			}
+			err = Copy(srcSubDir, dstEntryPath)
+			if err != nil {
+				return err
+			}
+		} else if entryInfo.Mode().IsRegular() {
+			var i int64
+			i, err = copyFile(entryInfo.Name(), src, dstEntryPath)
+			if err != nil {
+				return err
+			}
+			if i != entryInfo.Size() {
+				return errors.New("error writing to file")
+			}
+		}
+	}
+	return nil
+}
+
+func copyFile(name string, src DirFS, dst string) (int64, error) {
+	srcFile, err := src.Open(name)
+	if err != nil {
+		return 0, err
+	}
+	defer srcFile.Close()
+	dstFile, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer dstFile.Close()
+	return io.Copy(dstFile, srcFile)
 }
