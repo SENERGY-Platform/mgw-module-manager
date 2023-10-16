2e061d45b4b0533396c5b9b5d056c8590d64ab1c
y-du <42994541+y-du@users.noreply.github.com>
2023-10-16 17:00:58 +0200

    add sub-deployment tables

--- include/dep_storage_schema.sql
+++ include/dep_storage_schema.sql
@@ -93,3 +93,37 @@ CREATE TABLE IF NOT EXISTS `list_configs`
     PRIMARY KEY (`index`),
     FOREIGN KEY (`dep_id`) REFERENCES `deployments` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 );
+CREATE TABLE IF NOT EXISTS `sub_deployments`
+(
+    `index`   BIGINT AUTO_INCREMENT NOT NULL,
+    `id`      CHAR(36)              NOT NULL,
+    `dep_id`  CHAR(36)              NOT NULL,
+    `image`   VARCHAR(256)          NOT NULL,
+    `ctr_id`  VARCHAR(256)          NOT NULL,
+    `created` TIMESTAMP(6)          NOT NULL,
+    `updated` TIMESTAMP(6)          NOT NULL,
+    `name`    VARCHAR(256)          NULL,
+    UNIQUE KEY (`id`),
+    PRIMARY KEY (`index`),
+    FOREIGN KEY (`dep_id`) REFERENCES `deployments` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+);
+CREATE TABLE IF NOT EXISTS `sd_labels`
+(
+    `index` BIGINT AUTO_INCREMENT NOT NULL,
+    `sd_id` CHAR(36)              NOT NULL,
+    `key`   VARCHAR(256)          NOT NULL,
+    `value` VARCHAR(512),
+    UNIQUE KEY (`sd_id`, `key`),
+    PRIMARY KEY (`index`),
+    FOREIGN KEY (`sd_id`) REFERENCES `sub_deployments` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+);
+CREATE TABLE IF NOT EXISTS `sd_configs`
+(
+    `index` BIGINT AUTO_INCREMENT NOT NULL,
+    `sd_id` CHAR(36)              NOT NULL,
+    `ref`   VARCHAR(256)          NOT NULL,
+    `value` VARCHAR(512),
+    UNIQUE KEY (`sd_id`, `ref`),
+    PRIMARY KEY (`index`),
+    FOREIGN KEY (`sd_id`) REFERENCES `sub_deployments` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+);
