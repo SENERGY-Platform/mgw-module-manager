ec625809ff7551eec24fb5bb549be357ac85d194
y-du <42994541+y-du@users.noreply.github.com>
2023-10-16 15:36:12 +0200

    add sub-deployment model

---
+++ lib/model/sub_deployment.go
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package model
+
+import "time"
+
+type SubDepBase struct {
+	DepID  string            `json:"dep_id"`
+	Image  string            `json:"image"`
+	Labels map[string]string `json:"labels"`
+	Name   *string           `json:"name"`
+}
+
+type SubDeployment struct {
+	ID string `json:"id"` // uuid
+	SubDepBase
+	Ref     string    `json:"ref"`    // container name: mgw-sd- + SubDeployment:ID
+	CtrID   string    `json:"ctr_id"` // docker container id
+	Created time.Time `json:"created"`
+	Updated time.Time `json:"updated"`
+}
+
+type SubDepContainer struct {
+	ID      string `json:"id"`       // docker container id
+	ImageID string `json:"image_id"` // docker image id
+	State   string `json:"state"`    // docker container state
+}
+
+type SubDepRequest struct {
+	SubDepBase
+	EnvVars map[string]string `json:"env_vars"`
+}
+
+type SubDepFilter struct {
+	Labels map[string]string `json:"labels"`
+	Image  string            `json:"image"`
+	State  string            `json:"state"`
+}
