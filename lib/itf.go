743cbd08e9a5d532591ebaefc145025cdce120df
y-du <42994541+y-du@users.noreply.github.com>
2023-11-02 14:21:59 +0100

    add force delete deployment option

--- lib/itf.go
+++ lib/itf.go
@@ -40,7 +40,7 @@ type Api interface {
 	EnableDeployment(ctx context.Context, dID string) error
 	DisableDeployment(ctx context.Context, dID string, dependencies bool) (string, error)
 	UpdateDeployment(ctx context.Context, dID string, depInput model.DepInput) (string, error)
-	DeleteDeployment(ctx context.Context, dID string, orphans bool) error
+	DeleteDeployment(ctx context.Context, dID string, orphans, force bool) error
 	RestartDeployment(ctx context.Context, dID string) (string, error)
 	GetDeploymentUpdateTemplate(ctx context.Context, dID string) (model.DepUpdateTemplate, error)
 	GetDeploymentsHealth(ctx context.Context) (map[string]model.DepHealthInfo, error)
