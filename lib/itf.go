09b8344f14501f975b71629a92e83ddb565bcdec
y-du <42994541+y-du@users.noreply.github.com>
2023-11-03 16:18:39 +0100

    add start and stop methods

--- lib/itf.go
+++ lib/itf.go
@@ -41,6 +41,8 @@ type Api interface {
 	DisableDeployment(ctx context.Context, dID string) error
 	UpdateDeployment(ctx context.Context, dID string, depInput model.DepInput) (string, error)
 	DeleteDeployment(ctx context.Context, dID string, orphans, force bool) error
+	StartDeployment(ctx context.Context, dID string, dependencies bool) error
+	StopDeployment(ctx context.Context, dID string, dependencies bool) (string, error)
 	RestartDeployment(ctx context.Context, dID string) (string, error)
 	GetDeploymentUpdateTemplate(ctx context.Context, dID string) (model.DepUpdateTemplate, error)
 	GetDeploymentsHealth(ctx context.Context) (map[string]model.DepHealthInfo, error)
