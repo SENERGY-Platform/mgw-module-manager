2f379904b4e6c51d78db3ceb3c37ffadc9a0d674
y-du <42994541+y-du@users.noreply.github.com>
2023-11-10 11:43:08 +0100

    add naming handler

---
+++ util/naming_hdl/handler.go
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package naming_hdl
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"github.com/google/uuid"
+)
+
+var Global *handler
+
+type handler struct {
+	coreID string
+	prefix string
+}
+
+func Init(cID string, prefix string) {
+	Global = &handler{
+		coreID: cID,
+		prefix: prefix,
+	}
+}
+
+func (h *handler) NewContainerName(subPrefix string) (string, error) {
+	newUUID, err := uuid.NewUUID()
+	if err != nil {
+		return "", err
+	}
+	return fmt.Sprintf("%s-%s-%s", h.getPrefix(), subPrefix, hex.EncodeToString([]byte(newUUID.String()))), nil
+}
+
+func (h *handler) NewContainerAlias(arg ...string) string {
+	hash := sha1.New()
+	for _, s := range arg {
+		hash.Write([]byte(s))
+	}
+	return fmt.Sprintf("%s-%s", h.getPrefix(), hex.EncodeToString(hash.Sum(nil)))
+}
+
+func (h *handler) getPrefix() string {
+	return fmt.Sprintf("%s-%s", h.prefix, h.coreID)
+}
