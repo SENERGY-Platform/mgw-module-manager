9afaf71c5ee34fd97f9d46de9bf124f28f4e430a
y-du <42994541+y-du@users.noreply.github.com>
2023-11-10 08:24:00 +0100

    reimplement deployment storage handler

---
+++ handler/dep_storage_hdl/dep_assets.go
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2023 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dep_storage_hdl
+
+import (
+	"context"
+	"database/sql"
+	"database/sql/driver"
+	"github.com/SENERGY-Platform/mgw-module-manager/handler/dep_storage_hdl/dep_util"
+	"github.com/SENERGY-Platform/mgw-module-manager/lib/model"
+)
+
+func (h *Handler) CreateDepAssets(ctx context.Context, txItf driver.Tx, dID string, depAssets model.DepAssets) error {
+	var tx *sql.Tx
+	if txItf != nil {
+		tx = txItf.(*sql.Tx)
+	} else {
+		var e error
+		if tx, e = h.db.BeginTx(ctx, nil); e != nil {
+			return model.NewInternalError(e)
+		}
+		defer tx.Rollback()
+	}
+	if err := dep_util.InsertDepHostRes(ctx, tx, dID, depAssets.HostResources); err != nil {
+		return err
+	}
+	if err := dep_util.InsertDepSecrets(ctx, tx, dID, depAssets.Secrets); err != nil {
+		return err
+	}
+	if err := dep_util.InsertDepConfigs(ctx, tx, dID, depAssets.Configs); err != nil {
+		return err
+	}
+	if txItf == nil {
+		return tx.Commit()
+	}
+	return nil
+}
+
+func (h *Handler) DeleteDepAssets(ctx context.Context, txItf driver.Tx, dID string) error {
+	var tx *sql.Tx
+	if txItf != nil {
+		tx = txItf.(*sql.Tx)
+	} else {
+		var e error
+		if tx, e = h.db.BeginTx(ctx, nil); e != nil {
+			return model.NewInternalError(e)
+		}
+		defer tx.Rollback()
+	}
+	if err := dep_util.DeleteDepHostRes(ctx, tx, dID); err != nil {
+		return err
+	}
+	if err := dep_util.DeleteDepSecrets(ctx, tx, dID); err != nil {
+		return err
+	}
+	if err := dep_util.DeleteDepConfigs(ctx, tx, dID); err != nil {
+		return err
+	}
+	if txItf == nil {
+		return tx.Commit()
+	}
+	return nil
+}
