770000ee977c1d23fd9e5f866e2cb92d398115ca
y-du <42994541+y-du@users.noreply.github.com>
2023-11-14 15:23:23 +0100

    add module tables

--- include/dep_storage_schema.sql
+++ include/dep_storage_schema.sql
@@ -1,3 +1,24 @@
+CREATE TABLE IF NOT EXISTS `modules`
+(
+    `index`    BIGINT AUTO_INCREMENT NOT NULL,
+    `id`       VARCHAR(256)          NOT NULL,
+    `dir`      VARCHAR(256)          NOT NULL,
+    `modfile`  VARCHAR(16)           NOT NULL,
+    `indirect` BOOLEAN               NOT NULL,
+    `added`    TIMESTAMP(6)          NOT NULL,
+    `updated`  TIMESTAMP(6)          NOT NULL,
+    UNIQUE KEY (`id`),
+    PRIMARY KEY (`index`)
+);
+CREATE TABLE IF NOT EXISTS `mod_dependencies`
+(
+    `index`  BIGINT AUTO_INCREMENT NOT NULL,
+    `mod_id` CHAR(36)              NOT NULL,
+    `req_id` CHAR(36)              NOT NULL,
+    UNIQUE KEY (`mod_id`, `req_id`),
+    PRIMARY KEY (`index`),
+    FOREIGN KEY (`mod_id`) REFERENCES `modules` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+);
 CREATE TABLE IF NOT EXISTS `deployments`
 (
     `index`    BIGINT AUTO_INCREMENT NOT NULL,
@@ -11,7 +32,8 @@ CREATE TABLE IF NOT EXISTS `deployments`
     `created`  TIMESTAMP(6)          NOT NULL,
     `updated`  TIMESTAMP(6)          NOT NULL,
     UNIQUE KEY (`id`),
-    PRIMARY KEY (`index`)
+    PRIMARY KEY (`index`),
+    FOREIGN KEY (`mod_id`) REFERENCES `modules` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 );
 CREATE TABLE IF NOT EXISTS `dependencies`
 (
