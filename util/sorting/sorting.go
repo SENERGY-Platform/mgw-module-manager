afc5e78cc11b4a0634d2329bcdddd0564335c711
y-du <42994541+y-du@users.noreply.github.com>
2024-02-15 10:05:51 +0100

    fix GetModOrder removing modules with no dependencies

--- util/sorting/sorting.go
+++ util/sorting/sorting.go
@@ -26,13 +26,14 @@ func GetModOrder(modules map[string]*module.Module) (order []string, err error) 
 	if len(modules) > 1 {
 		nodes := make(tsort.Nodes)
 		for _, m := range modules {
+			var reqIDs map[string]struct{}
 			if len(m.Dependencies) > 0 {
-				reqIDs := make(map[string]struct{})
+				reqIDs = make(map[string]struct{})
 				for i := range m.Dependencies {
 					reqIDs[i] = struct{}{}
 				}
-				nodes.Add(m.ID, reqIDs, nil)
 			}
+			nodes.Add(m.ID, reqIDs, nil)
 		}
 		order, err = tsort.GetTopOrder(nodes)
 		if err != nil {
