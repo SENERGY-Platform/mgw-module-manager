a099b1a3b708eb40d17f80e610a49f81c2f24fa3
y-du <42994541+y-du@users.noreply.github.com>
2024-06-11 08:58:09 +0200

    fix

--- include/dep_storage_schema.sql
+++ include/dep_storage_schema.sql
@@ -171,6 +171,7 @@ CREATE TABLE IF NOT EXISTS `dep_advertisements`
     `origin`    VARCHAR(256)          NOT NULL,
     `ref`       VARCHAR(256)          NOT NULL,
     `timestamp` TIMESTAMP(6)          NOT NULL,
+    UNIQUE KEY (`id`),
     UNIQUE KEY (`dep_id`, `ref`),
     PRIMARY KEY (`index`),
     FOREIGN KEY (`dep_id`) REFERENCES `deployments` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
@@ -181,7 +182,7 @@ CREATE TABLE IF NOT EXISTS `dep_adv_items`
     `adv_id` CHAR(36)              NOT NULL,
     `key`    VARCHAR(256)          NOT NULL,
     `value`  VARCHAR(512),
-    UNIQUE KEY (adv_id, `key`),
+    UNIQUE KEY (`adv_id`, `key`),
     PRIMARY KEY (`index`),
     FOREIGN KEY (adv_id) REFERENCES `dep_advertisements` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 );
