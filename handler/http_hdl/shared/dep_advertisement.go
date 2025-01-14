f584cc6d223f376bf85df6b2f82c392c8bc0d66f
y-du <42994541+y-du@users.noreply.github.com>
2025-01-14 13:11:38 +0100

    refactor

---
+++ handler/http_hdl/shared/dep_advertisement.go
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2025 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package shared
+
+import (
+	"github.com/SENERGY-Platform/mgw-module-manager/lib"
+	lib_model "github.com/SENERGY-Platform/mgw-module-manager/lib/model"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type advertisementFilterQuery struct {
+	ModuleID string `form:"module_id"`
+	Origin   string `form:"origin"`
+	Ref      string `form:"ref"`
+}
+
+func getDepAdvertisementQueryH(a lib.Api) (string, string, gin.HandlerFunc) {
+	return http.MethodGet, lib_model.DiscoveryPath, func(gc *gin.Context) {
+		var query advertisementFilterQuery
+		if err := gc.ShouldBindQuery(&query); err != nil {
+			_ = gc.Error(lib_model.NewInvalidInputError(err))
+			return
+		}
+		ads, err := a.QueryDepAdvertisements(gc.Request.Context(), lib_model.DepAdvFilter{
+			ModuleID: query.ModuleID,
+			Origin:   query.Origin,
+			Ref:      query.Ref,
+		})
+		if err != nil {
+			_ = gc.Error(err)
+			return
+		}
+		gc.JSON(http.StatusOK, ads)
+	}
+}
