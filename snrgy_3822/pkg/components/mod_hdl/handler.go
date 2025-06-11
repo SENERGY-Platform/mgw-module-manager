9685b686f4754b6f776edd8494a26972c2a337be
y-du <42994541+y-du@users.noreply.github.com>
2025-06-11 15:25:54 +0200

    implement methods

--- snrgy_3822/pkg/components/mod_hdl/handler.go
+++ snrgy_3822/pkg/components/mod_hdl/handler.go
@@ -2,33 +2,126 @@ package mod_hdl
 
 import (
 	"context"
+	"errors"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/components/fs_util"
+	models_error "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/error"
+	models_storage "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/storage"
+	"github.com/google/uuid"
 	"io/fs"
 	"os"
+	"path"
 	"sync"
+	"time"
 )
 
 type Handler struct {
-	wrkPath string
-	mu      sync.RWMutex
+	storageHdl  StorageHandler
+	workDirPath string
+	dbTimeout   time.Duration
+	mu          sync.RWMutex
 }
 
-func New(wrkPath string) *Handler {
+func New(storageHdl StorageHandler, workDirPath string, dbTimeout time.Duration) *Handler {
 	return &Handler{
-		wrkPath: wrkPath,
+		storageHdl:  storageHdl,
+		workDirPath: workDirPath,
+		dbTimeout:   dbTimeout,
 	}
 }
 
 func (h *Handler) Init() error {
 	h.mu.Lock()
 	defer h.mu.Unlock()
-	if err := os.MkdirAll(h.wrkPath, 0775); err != nil {
+	if err := os.MkdirAll(h.workDirPath, 0775); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (h *Handler) Add(_ context.Context, modID string, modFS fs.FS) error {
+func (h *Handler) Add(ctx context.Context, id string, fSys fs.FS) error {
 	h.mu.Lock()
 	defer h.mu.Unlock()
-	panic("not implemented")
+	ctxWt, cf := context.WithTimeout(ctx, h.dbTimeout)
+	defer cf()
+	if tmp, err := h.storageHdl.ReadMod(ctxWt, id); err != nil {
+		var notFoundErr *models_error.NotFoundError
+		if !errors.As(err, &notFoundErr) {
+			return err
+		}
+	} else {
+		if tmp.ID != "" {
+			return errors.New("already exists")
+		}
+	}
+	newUUID, err := uuid.NewUUID()
+	if err != nil {
+		return err
+	}
+	mod := models_storage.ModuleBase{
+		ID:      id,
+		DirName: newUUID.String(),
+	}
+	dstPath := path.Join(h.workDirPath, mod.DirName)
+	if err = fs_util.CopyAll(fSys, dstPath); err != nil {
+		return err
+	}
+	ctxWt2, cf2 := context.WithTimeout(ctx, h.dbTimeout)
+	defer cf2()
+	if err = h.storageHdl.CreateMod(ctxWt2, nil, mod); err != nil {
+		os.RemoveAll(dstPath)
+		return err
+	}
+	return nil
+}
+
+func (h *Handler) Update(ctx context.Context, id string, fSys fs.FS) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	ctxWt, cf := context.WithTimeout(ctx, h.dbTimeout)
+	defer cf()
+	oldMod, err := h.storageHdl.ReadMod(ctxWt, id)
+	if err != nil {
+		return err
+	}
+	newUUID, err := uuid.NewUUID()
+	if err != nil {
+		return err
+	}
+	newMod := models_storage.ModuleBase{
+		ID:      id,
+		DirName: newUUID.String(),
+	}
+	dstPath := path.Join(h.workDirPath, newMod.DirName)
+	if err = fs_util.CopyAll(fSys, dstPath); err != nil {
+		return err
+	}
+	ctxWt2, cf2 := context.WithTimeout(ctx, h.dbTimeout)
+	defer cf2()
+	if err = h.storageHdl.UpdateMod(ctxWt2, nil, newMod); err != nil {
+		os.RemoveAll(dstPath)
+		return err
+	}
+	os.RemoveAll(path.Join(h.workDirPath, oldMod.DirName))
+	return nil
+}
+
+func (h *Handler) Delete(ctx context.Context, id string) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	ctxWt, cf := context.WithTimeout(ctx, h.dbTimeout)
+	defer cf()
+	mod, err := h.storageHdl.ReadMod(ctxWt, id)
+	if err != nil {
+		return err
+	}
+	err = os.RemoveAll(path.Join(h.workDirPath, mod.DirName))
+	if err != nil && !os.IsNotExist(err) {
+		return err
+	}
+	ctxWt2, cf2 := context.WithTimeout(ctx, h.dbTimeout)
+	defer cf2()
+	if err = h.storageHdl.DeleteMod(ctxWt2, nil, id); err != nil {
+		return err
+	}
+	return nil
 }
