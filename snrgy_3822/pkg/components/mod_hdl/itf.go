a1c14d846250656618a80825c2af5cce1dbe3bee
y-du <42994541+y-du@users.noreply.github.com>
2025-06-12 15:43:11 +0200

    add storage handler interface

---
+++ snrgy_3822/pkg/components/mod_hdl/itf.go
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2025 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package mod_hdl
+
+import (
+	"context"
+	models_storage "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/storage"
+)
+
+type StorageHandler interface {
+	ListMod(ctx context.Context, filter models_storage.ModuleFilter) (map[string]models_storage.Module, error)
+	ReadMod(ctx context.Context, id string) (models_storage.Module, error)
+	CreateMod(ctx context.Context, mod models_storage.ModuleBase) error
+	UpdateMod(ctx context.Context, mod models_storage.ModuleBase) error
+	DeleteMod(ctx context.Context, id string) error
+}
