d6b945d44619064755fd9060877421506e6b21ea
y-du <42994541+y-du@users.noreply.github.com>
2025-06-13 15:35:59 +0200

    refactor and fix dir tree

--- snrgy_3822/pkg/components/github_mod_repo_hdl/handler.go
+++ snrgy_3822/pkg/components/github_mod_repo_hdl/handler.go
@@ -82,18 +82,19 @@ func (h *Handler) DefaultChannel() string {
 	return ""
 }
 
-func (h *Handler) FileSystemsMap(ctx context.Context, channelID string) (map[string]fs.FS, error) {
+func (h *Handler) FileSystemsMap(ctx context.Context, channelName string) (map[string]fs.FS, error) {
 	h.mu.RLock()
 	defer h.mu.RUnlock()
-	channel, ok := h.channels[channelID]
+	channel, ok := h.channels[channelName]
 	if !ok {
 		return nil, errors.New("channel does not exist")
 	}
-	repo, err := readRepoFile(path.Join(h.wrkPath, channel.Name))
+	basePath := path.Join(h.wrkPath, channel.Name)
+	repo, err := readRepoFile(basePath)
 	if err != nil {
 		return nil, err
 	}
-	dirEntries, err := os.ReadDir(path.Join(h.wrkPath, channel.Name, repo.DirName))
+	dirEntries, err := os.ReadDir(path.Join(basePath, repo.Path))
 	if err != nil {
 		return nil, err
 	}
@@ -103,24 +104,25 @@ func (h *Handler) FileSystemsMap(ctx context.Context, channelID string) (map[str
 			return nil, ctx.Err()
 		}
 		if entry.IsDir() && !inSlice(commonBlacklist, entry.Name()) && !inSlice(channel.Blacklist, entry.Name()) {
-			fsMap[entry.Name()] = os.DirFS(path.Join(h.wrkPath, channel.Name, repo.DirName, entry.Name()))
+			fsMap[entry.Name()] = os.DirFS(path.Join(basePath, repo.Path, entry.Name()))
 		}
 	}
 	return fsMap, nil
 }
 
-func (h *Handler) FileSystem(ctx context.Context, channelID, fsRef string) (fs.FS, error) {
+func (h *Handler) FileSystem(ctx context.Context, channelName, fsRef string) (fs.FS, error) {
 	h.mu.RLock()
 	defer h.mu.RUnlock()
-	channel, ok := h.channels[channelID]
+	channel, ok := h.channels[channelName]
 	if !ok {
 		return nil, errors.New("channel does not exist")
 	}
-	repo, err := readRepoFile(path.Join(h.wrkPath, channel.Name))
+	basePath := path.Join(h.wrkPath, channel.Name)
+	repo, err := readRepoFile(basePath)
 	if err != nil {
 		return nil, err
 	}
-	dirEntries, err := os.ReadDir(path.Join(h.wrkPath, channel.Name, repo.DirName))
+	dirEntries, err := os.ReadDir(path.Join(basePath, repo.Path))
 	if err != nil {
 		return nil, err
 	}
@@ -129,7 +131,7 @@ func (h *Handler) FileSystem(ctx context.Context, channelID, fsRef string) (fs.F
 			return nil, ctx.Err()
 		}
 		if entry.IsDir() && entry.Name() == fsRef {
-			return os.DirFS(path.Join(h.wrkPath, channel.Name, repo.DirName, entry.Name())), nil
+			return os.DirFS(path.Join(basePath, repo.Path, entry.Name())), nil
 		}
 	}
 	return nil, errors.New("reference does not exist")
@@ -151,7 +153,8 @@ func (h *Handler) Refresh(ctx context.Context) error {
 }
 
 func (h *Handler) refresh(ctx context.Context, channel Channel) error {
-	oldRepo, err := readRepoFile(path.Join(h.wrkPath, channel.Name))
+	basePath := path.Join(h.wrkPath, channel.Name)
+	oldRepo, err := readRepoFile(basePath)
 	if err != nil {
 		return err
 	}
@@ -172,19 +175,24 @@ func (h *Handler) refresh(ctx context.Context, channel Channel) error {
 		return err
 	}
 	defer repoArchive.Close()
-	newRepo.DirName, err = archive_util.ExtractTarGz(repoArchive, path.Join(h.wrkPath, channel.Name))
+	if err = os.MkdirAll(path.Join(basePath, newRepo.GitCommit.Sha), 0775); err != nil {
+		return err
+	}
+	rootDir, err := archive_util.ExtractTarGz(repoArchive, path.Join(basePath, newRepo.GitCommit.Sha))
 	if err != nil {
 		_, _ = io.ReadAll(repoArchive)
 		return err
 	}
-	if err = writeRepoFile(path.Join(h.wrkPath, channel.Name), newRepo); err != nil {
-		if e := os.RemoveAll(path.Join(path.Join(h.wrkPath, channel.Name), newRepo.DirName)); e != nil {
+	newRepo.Path = path.Join(newRepo.GitCommit.Sha, rootDir)
+	if err = writeRepoFile(basePath, newRepo); err != nil {
+		if e := os.RemoveAll(path.Join(basePath, newRepo.GitCommit.Sha)); e != nil {
 			return errors.Join(err, e)
 		}
 		return err
 	}
-	if oldRepo.DirName != "" && oldRepo.DirName != newRepo.DirName {
-		if e := os.RemoveAll(path.Join(path.Join(h.wrkPath, channel.Name), oldRepo.DirName)); e != nil {
+	if oldRepo.Path != "" && oldRepo.Path != newRepo.Path {
+		fmt.Println(path.Join(basePath, oldRepo.Path))
+		if e := os.RemoveAll(path.Join(basePath, oldRepo.GitCommit.Sha)); e != nil {
 			fmt.Println(e)
 		}
 	}
