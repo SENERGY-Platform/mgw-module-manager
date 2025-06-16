7f02e5ee2f5f574e7bd67e27852b07b7f599b616
y-du <42994541+y-du@users.noreply.github.com>
2025-06-16 13:04:07 +0200

    init

---
+++ snrgy_3822/pkg/components/logger/logger.go
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2025 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package logger
+
+import (
+	sb_logger "github.com/SENERGY-Platform/go-service-base/logger"
+	"github.com/y-du/go-log-level"
+	"os"
+)
+
+var Logger *log_level.Logger
+
+func Init(c Config) (out *os.File, err error) {
+	Logger, out, err = sb_logger.New(c.Level, c.Path, c.FileName, c.Prefix, c.Utc, c.Terminal, c.Microseconds)
+	Logger.SetLevelPrefix("ERROR ", "WARNING ", "INFO ", "DEBUG ")
+	return
+}
