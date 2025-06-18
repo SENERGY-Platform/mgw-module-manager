f0b865a086b6a00bd5632dd37cbf1bc3dbea715b
y-du <42994541+y-du@users.noreply.github.com>
2025-06-18 16:40:37 +0200

    init

---
+++ snrgy_3822/pkg/models/service/service.go
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2025 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package service
+
+type RepoModule struct {
+	ID               string         `json:"id"`
+	Name             string         `json:"name"`
+	Desc             string         `json:"description"`
+	Version          string         `json:"version"`
+	Repositories     []Repository   `json:"repositories"`
+	InstalledVariant *ModuleVariant `json:"installed_variant"`
+}
+
+type Repository struct {
+	Source   string    `json:"source"`
+	Default  bool      `json:"default"`
+	Channels []Channel `json:"channels"`
+}
+
+type Channel struct {
+	Name    string `json:"name"`
+	Default bool   `json:"default"`
+	Version string `json:"version"`
+}
+
+type ModuleVariant struct {
+	Source  string `json:"source"`
+	Channel string `json:"channel"`
+	Version string `json:"version"`
+}
