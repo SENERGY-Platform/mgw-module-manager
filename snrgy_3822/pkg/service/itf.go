26983e93a03504e3a2643cebd1b35ed13746a7b6
y-du <42994541+y-du@users.noreply.github.com>
2025-06-18 16:41:22 +0200

    update and add interfaces

--- snrgy_3822/pkg/service/itf.go
+++ snrgy_3822/pkg/service/itf.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	models_module "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/module"
 	models_repo "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/repository"
 	"io/fs"
 )
@@ -9,7 +10,21 @@ import (
 type ModuleReposHandler interface {
 	RefreshRepositories(ctx context.Context) error
 	SetDefaultRepository(source string) error
-	Repositories(ctx context.Context) (map[string]models_repo.Repository, error)
+	Repositories(ctx context.Context) ([]models_repo.Repository, error)
+	Module(ctx context.Context, id, source, channel string) (models_repo.Module, error)
 	Modules(ctx context.Context) ([]models_repo.Module, error)
 	ModuleFS(ctx context.Context, id, source, channel string) (fs.FS, error)
+}
+
+type ModuleHandler interface {
+	Modules(ctx context.Context, filter models_module.ModuleFilter) ([]models_module.ModuleAbbreviated, error)
+	Module(ctx context.Context, id string) (models_module.Module, error)
+	ModuleFS(ctx context.Context, id string) (fs.FS, error)
+	Add(ctx context.Context, id, source, channel string, fSys fs.FS) error
+	Update(ctx context.Context, id, source, channel string, fSys fs.FS) error
+	Remove(ctx context.Context, id string) error
+}
+
+type Logger interface {
+	Error(v ...any)
 }
