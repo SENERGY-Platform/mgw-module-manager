2ee918446520e63f5e29bb517b6dc8d9beb3a7f2
y-du <42994541+y-du@users.noreply.github.com>
2025-07-25 10:23:04 +0200

    init helper

---
+++ snrgy_3822/pkg/components/helper/http/http.go
@@ -0,0 +1,23 @@
+package http
+
+import (
+	"net"
+	"net/http"
+	"time"
+)
+
+func NewClient(timeout time.Duration) *http.Client {
+	return &http.Client{
+		Timeout: timeout,
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+		},
+	}
+}
