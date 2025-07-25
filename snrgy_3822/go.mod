2c65a6555d7cade32c7aa09113e3b16fee824eb7
y-du <42994541+y-du@users.noreply.github.com>
2025-07-25 10:23:18 +0200

    init main

--- snrgy_3822/go.mod
+++ snrgy_3822/go.mod
@@ -3,12 +3,17 @@ module github.com/SENERGY-Platform/mgw-module-manager
 go 1.24.2
 
 require (
+	github.com/SENERGY-Platform/gin-middleware v0.9.0
 	github.com/SENERGY-Platform/go-service-base/config-hdl v1.2.0
+	github.com/SENERGY-Platform/go-service-base/srv-info-hdl v0.2.0
 	github.com/SENERGY-Platform/go-service-base/struct-logger v0.4.1
 	github.com/SENERGY-Platform/mgw-container-engine-wrapper/lib v0.17.0
 	github.com/SENERGY-Platform/mgw-go-service-base/job-hdl/lib v0.1.1
 	github.com/SENERGY-Platform/mgw-modfile-lib v0.20.0
 	github.com/SENERGY-Platform/mgw-module-lib v0.23.0
+	github.com/SENERGY-Platform/mgw-module-manager/lib v0.0.0-00000000000000-000000000000
+	github.com/gin-contrib/requestid v1.0.5
+	github.com/gin-gonic/gin v1.10.1
 	github.com/google/uuid v1.6.0
 	gopkg.in/yaml.v3 v3.0.1
 )
@@ -16,4 +21,30 @@ require (
 require (
 	code.cloudfoundry.org/bytefmt v0.31.0 // indirect
 	github.com/SENERGY-Platform/go-env-loader v0.5.3 // indirect
+	github.com/bytedance/sonic v1.13.2 // indirect
+	github.com/bytedance/sonic/loader v0.2.4 // indirect
+	github.com/cloudwego/base64x v0.1.5 // indirect
+	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/gin-contrib/sse v1.0.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/go-playground/validator/v10 v10.26.0 // indirect
+	github.com/goccy/go-json v0.10.5 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.10 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.3 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
+	golang.org/x/arch v0.15.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+	golang.org/x/net v0.38.0 // indirect
+	golang.org/x/sys v0.31.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+	google.golang.org/protobuf v1.36.6 // indirect
 )
+
+replace github.com/SENERGY-Platform/mgw-module-manager/lib => ./lib
--- snrgy_3822/main.go
+++ snrgy_3822/main.go
@@ -1,5 +1,100 @@
 package main
 
-func main() {
+import (
+	"context"
+	"errors"
+	"fmt"
+	sb_config_hdl "github.com/SENERGY-Platform/go-service-base/config-hdl"
+	"github.com/SENERGY-Platform/go-service-base/srv-info-hdl"
+	struct_logger "github.com/SENERGY-Platform/go-service-base/struct-logger"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/api"
+	helper_os_signal "github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/os_signal"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/config"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/slog_attr"
+	"net"
+	"net/http"
+	"os"
+	"strconv"
+	"sync"
+	"syscall"
+	"time"
+)
 
+var version string
+
+func main() {
+	ec := 0
+	defer func() {
+		os.Exit(ec)
+	}()
+
+	srvInfoHdl := srv_info_hdl.New("cert-manager", version)
+
+	config.ParseFlags()
+
+	cfg, err := config.New(config.ConfPath)
+	if err != nil {
+		_, _ = fmt.Fprintln(os.Stderr, err)
+		ec = 1
+		return
+	}
+
+	logger := struct_logger.New(cfg.Logger, os.Stderr, "", srvInfoHdl.Name())
+
+	logger.Info("starting service", slog_attr.VersionKey, srvInfoHdl.Version(), slog_attr.ConfigValuesKey, sb_config_hdl.StructToMap(cfg, true))
+
+	httpApi, err := api.New(
+		nil,
+		srvInfoHdl,
+		logger,
+		cfg.HttpAccessLog,
+	)
+	if err != nil {
+		logger.Error("creating http engine failed", slog_attr.ErrorKey, err)
+		ec = 1
+		return
+	}
+
+	httpServer := &http.Server{Handler: httpApi.Handler()}
+	serverListener, err := net.Listen("tcp", ":"+strconv.FormatInt(int64(cfg.ServerPort), 10))
+	if err != nil {
+		logger.Error("creating server listener failed", slog_attr.ErrorKey, err)
+		ec = 1
+		return
+	}
+
+	ctx, cf := context.WithCancel(context.Background())
+
+	go func() {
+		helper_os_signal.Wait(ctx, logger, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+		cf()
+	}()
+
+	wg := &sync.WaitGroup{}
+
+	go func() {
+		logger.Info("starting http server")
+		if err := httpServer.Serve(serverListener); !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("starting server failed", slog_attr.ErrorKey, err)
+			ec = 1
+		}
+		cf()
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+		logger.Info("stopping http server")
+		ctxWt, cf2 := context.WithTimeout(context.Background(), time.Second*5)
+		defer cf2()
+		if err := httpServer.Shutdown(ctxWt); err != nil {
+			logger.Error("stopping server failed", slog_attr.ErrorKey, err)
+			ec = 1
+		} else {
+			logger.Info("http server stopped")
+		}
+	}()
+
+	wg.Wait()
 }
