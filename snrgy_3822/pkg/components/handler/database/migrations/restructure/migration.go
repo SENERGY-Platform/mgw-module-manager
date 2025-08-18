8cdd7babc08206695f90d864d19978a640e8313b
y-du <42994541+y-du@users.noreply.github.com>
2025-08-18 13:19:58 +0200

    add aux_deployment table migration

--- snrgy_3822/pkg/components/handler/database/migrations/restructure/migration.go
+++ snrgy_3822/pkg/components/handler/database/migrations/restructure/migration.go
@@ -44,5 +44,9 @@ func (m *Migration) Run(ctx context.Context, db *sql.DB) error {
 	if err != nil {
 		return err
 	}
+	err = migrateAuxDeployments(ctx, db)
+	if err != nil {
+		return err
+	}
 	return nil
 }
