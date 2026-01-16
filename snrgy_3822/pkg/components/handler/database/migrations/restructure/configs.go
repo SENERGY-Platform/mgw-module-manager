e30016cb3d380c52c5890074390ef32936ff1fe2
y-du <42994541+y-du@users.noreply.github.com>
2026-01-16 15:30:29 +0100

    fix deployment configs migration after table changes

--- snrgy_3822/pkg/components/handler/database/migrations/restructure/configs.go
+++ snrgy_3822/pkg/components/handler/database/migrations/restructure/configs.go
@@ -19,8 +19,51 @@ package restructure
 import (
 	"context"
 	"database/sql"
+	"errors"
+	"fmt"
+	"maps"
 	"slices"
+
+	models_handler_storage "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/storage"
 )
+
+func createDepConfigTables(ctx context.Context, db *sql.DB) error {
+	_, err := db.ExecContext(ctx,
+		`CREATE TABLE IF NOT EXISTS dep_configs
+(
+id        VARCHAR(256) NOT NULL,
+dep_id    CHAR(36)     NOT NULL,
+ref       VARCHAR(128) NOT NULL,
+data_type SMALLINT     NOT NULL,
+is_list   BOOLEAN      NOT NULL,
+PRIMARY KEY (id),
+UNIQUE KEY uk_dep_id_ref (dep_id, ref),
+INDEX i_dep_id (dep_id),
+FOREIGN KEY (dep_id) REFERENCES deployments (id) ON DELETE CASCADE ON UPDATE RESTRICT
+);`,
+	)
+	if err != nil {
+		return err
+	}
+	_, err = db.ExecContext(ctx,
+		`CREATE TABLE IF NOT EXISTS dep_config_values
+(
+c_id     VARCHAR(256) NOT NULL,
+v_string VARCHAR(512),
+v_int    BIGINT,
+v_float  DOUBLE,
+v_bool   BOOLEAN,
+ord      SMALLINT     NOT NULL,
+UNIQUE KEY uk_c_id_ord (c_id, ord),
+INDEX i_id (c_id),
+FOREIGN KEY (c_id) REFERENCES dep_configs (id) ON DELETE CASCADE ON UPDATE RESTRICT
+);`,
+	)
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func migrateConfigsTab(ctx context.Context, db *sql.DB) error {
 	tableName := "configs"
@@ -31,56 +74,225 @@ func migrateConfigsTab(ctx context.Context, db *sql.DB) error {
 	if !ok {
 		return nil
 	}
-	ok, err = columnExists(ctx, db, tableName, "index")
+	configs, err := queryConfigs(ctx, db)
 	if err != nil {
 		return err
 	}
-	if ok {
-		logger.Info("dropping column", attrColumn, "index", attrTable, tableName)
-		err = dropColumn(ctx, db, tableName, "`index`")
+	if len(configs) == 0 {
+		return nil
+	}
+	logger.Info("transforming data from table", attrTable, tableName)
+	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+	for _, c := range configs {
+		cId := c.DepId + "_" + c.Ref
+		_, err = tx.ExecContext(
+			ctx,
+			"INSERT INTO dep_configs (id, dep_id, ref, data_type, is_list) VALUES (?, ?, ?, ?, ?)",
+			cId,
+			c.DepId,
+			c.Ref,
+			c.DT,
+			false,
+		)
+		if err != nil {
+			return err
+		}
+		_, err = tx.ExecContext(
+			ctx,
+			fmt.Sprintf("INSERT INTO dep_config_values (c_id, %s, ord) VALUES (?, ?, ?)", getColName(c.DT)),
+			cId,
+			c.Val,
+			0,
+		)
 		if err != nil {
 			return err
 		}
 	}
-	ok, err = indexExists(ctx, db, tableName, "uk_dep_id_ref")
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+	logger.Info("dropping table", attrTable, tableName)
+	return dropTable(ctx, db, tableName)
+}
+
+func migrateListConfigsTab(ctx context.Context, db *sql.DB) error {
+	tableName := "list_configs"
+	ok, err := tableExists(ctx, db, tableName)
 	if err != nil {
 		return err
 	}
 	if !ok {
-		logger.Info("adding unique index", attrIndex, "uk_dep_id_ref", attrTable, tableName)
-		err = addUniqueIndex(ctx, db, tableName, "uk_dep_id_ref", "dep_id", "ref")
-		if err != nil {
-			return err
-		}
+		return nil
 	}
-	ok, err = indexExists(ctx, db, tableName, "i_dep_id")
+	configs, err := queryListConfigs(ctx, db)
 	if err != nil {
 		return err
 	}
-	if !ok {
-		logger.Info("adding index", attrIndex, "i_dep_id", attrTable, tableName)
-		err = addIndex(ctx, db, tableName, "i_dep_id", "dep_id")
-		if err != nil {
-			return err
-		}
+	if len(configs) == 0 {
+		return nil
 	}
-	currentIndexKeys, err := indexKeyNames(ctx, db, tableName)
+	logger.Info("transforming data from table", attrTable, tableName)
+	tx, err := db.BeginTx(ctx, nil)
 	if err != nil {
 		return err
 	}
-	newIndexKeys := []string{"uk_dep_id_ref", "i_dep_id"}
-	for _, key := range currentIndexKeys {
-		if key == "PRIMARY" {
-			continue
+	defer tx.Rollback()
+	for _, c := range configs {
+		cId := c.DepId + "_" + c.Ref
+		_, err = tx.ExecContext(
+			ctx,
+			"INSERT INTO dep_configs (id, dep_id, ref, data_type, is_list) VALUES (?, ?, ?, ?, ?)",
+			cId,
+			c.DepId,
+			c.Ref,
+			c.DT,
+			false,
+		)
+		if err != nil {
+			return err
 		}
-		if !slices.Contains(newIndexKeys, key) {
-			logger.Info("dropping index", attrIndex, key, attrTable, tableName)
-			err = dropIndex(ctx, db, tableName, key)
+		stmt := fmt.Sprintf("INSERT INTO dep_config_values (c_id, %s, ord) VALUES (?, ?, ?)", getColName(c.DT))
+		for i, val := range c.Vals {
+			_, err = tx.ExecContext(ctx, stmt, cId, val, i)
 			if err != nil {
 				return err
 			}
 		}
 	}
-	logger.Info("renaming table", attrTable, tableName, attrNewName, "dep_configs")
-	return renameTable(ctx, db, tableName, "dep_configs")
+	err = tx.Commit()
+	if err != nil {
+		return err
+	}
+	logger.Info("dropping table", attrTable, tableName)
+	return dropTable(ctx, db, tableName)
+}
+
+type config struct {
+	DepId string
+	Ref   string
+	DT    int
+	Val   any
+}
+
+type listConfig struct {
+	DepId string
+	Ref   string
+	DT    int
+	Vals  []any
+}
+
+func queryConfigs(ctx context.Context, db *sql.DB) ([]config, error) {
+	rows, err := db.QueryContext(ctx, "SELECT dep_id, ref, v_string, v_int, v_float, v_bool FROM configs")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var configs []config
+	for rows.Next() {
+		var depId string
+		var ref string
+		var vString sql.NullString
+		var vInt sql.NullInt64
+		var vFloat sql.NullFloat64
+		var vBool sql.NullBool
+		if err = rows.Scan(&depId, &ref, &vString, &vInt, &vFloat, &vBool); err != nil {
+			return nil, err
+		}
+		c := config{
+			DepId: depId,
+			Ref:   ref,
+		}
+		if vString.Valid {
+			c.Val = vString.String
+			c.DT = models_handler_storage.StringType
+		} else if vInt.Valid {
+			c.Val = vInt.Int64
+			c.DT = models_handler_storage.Int64Type
+		} else if vFloat.Valid {
+			c.Val = vFloat.Float64
+			c.DT = models_handler_storage.Float64Type
+		} else if vBool.Valid {
+			c.Val = vBool.Bool
+			c.DT = models_handler_storage.BoolType
+		}
+		configs = append(configs, c)
+	}
+	return configs, nil
+}
+
+func queryListConfigs(ctx context.Context, db *sql.DB) ([]listConfig, error) {
+	rows, err := db.QueryContext(ctx, "SELECT dep_id, ref, ord, v_string, v_int, v_float, v_bool FROM list_configs ORDER BY dep_id, ref, ord")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	listConfigsMap := make(map[string]map[string]listConfig)
+	for rows.Next() {
+		var depId string
+		var ref string
+		var ord int
+		var vString sql.NullString
+		var vInt sql.NullInt64
+		var vFloat sql.NullFloat64
+		var vBool sql.NullBool
+		if err = rows.Scan(&depId, &ref, &ord, &vString, &vInt, &vFloat, &vBool); err != nil {
+			return nil, err
+		}
+		configsMap, ok := listConfigsMap[depId]
+		if !ok {
+			configsMap = make(map[string]listConfig)
+			listConfigsMap[depId] = configsMap
+		}
+		lc, ok := configsMap[ref]
+		if !ok {
+			lc.DepId = depId
+			lc.Ref = ref
+		}
+		var dt int
+		if vString.Valid {
+			lc.Vals = append(lc.Vals, vString.String)
+			dt = models_handler_storage.StringType
+		} else if vInt.Valid {
+			lc.Vals = append(lc.Vals, vInt.Int64)
+			dt = models_handler_storage.Int64Type
+		} else if vFloat.Valid {
+			lc.Vals = append(lc.Vals, vFloat.Float64)
+			dt = models_handler_storage.Float64Type
+		} else if vBool.Valid {
+			lc.Vals = append(lc.Vals, vBool.Bool)
+			dt = models_handler_storage.BoolType
+		}
+		if !ok {
+			lc.DT = dt
+		} else {
+			if dt != lc.DT {
+				return nil, errors.New("mismatched datatype")
+			}
+		}
+		configsMap[ref] = lc
+	}
+	var listConfigs []listConfig
+	for _, configsMap := range listConfigsMap {
+		listConfigs = append(listConfigs, slices.Collect(maps.Values(configsMap))...)
+	}
+	return listConfigs, nil
+}
+
+func getColName(dt int) string {
+	switch dt {
+	case models_handler_storage.StringType:
+		return "v_string"
+	case models_handler_storage.Int64Type:
+		return "v_int"
+	case models_handler_storage.Float64Type:
+		return "v_float"
+	case models_handler_storage.BoolType:
+		return "v_bool"
+	}
+	return ""
 }
