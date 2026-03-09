6c2a21aa7f0f906e68a588ad16cafed09ee69422
y-du <42994541+y-du@users.noreply.github.com>
2026-03-09 09:25:04 +0100

    add methods for pulling container images

---
+++ snrgy_3822/pkg/components/handler/deployments/images.go
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2026 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package deployments
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	helper_job "github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/job"
+	helper_url "github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/url"
+	models_external "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/external"
+)
+
+func (h *Handler) addDeploymentContainerImages(ctx context.Context, moduleServices map[string]models_external.ModuleService) error {
+	imageNames := make(map[string]struct{})
+	for _, service := range moduleServices {
+		imageNames[service.Image] = struct{}{}
+	}
+	var errs []string
+	for imageName := range imageNames {
+		err := h.addContainerImage(ctx, imageName)
+		if err != nil {
+			errs = append(errs, err.Error())
+		}
+	}
+	if len(errs) > 0 {
+		return errors.New(strings.Join(errs, "\n")) // TODO
+	}
+	return nil
+}
+
+func (h *Handler) addContainerImage(ctx context.Context, imageName string) error {
+	_, err := h.cewClient.GetImage(ctx, helper_url.EscapePath(imageName, h.config.PathEscapeDepth))
+	if err != nil {
+		var notFoundErr *models_external.CEWNotFoundErr
+		if !errors.As(err, &notFoundErr) {
+			return err
+		}
+	} else {
+		return nil
+	}
+	jobId, err := h.cewClient.AddImage(ctx, imageName)
+	if err != nil {
+		return err
+	}
+	job, err := helper_job.Await(ctx, h.cewClient, jobId, h.config.JobPollInterval)
+	if err != nil {
+		return err
+	}
+	if job.Error != nil {
+		return errors.New(job.Error.Message)
+	}
+	return nil
+}
