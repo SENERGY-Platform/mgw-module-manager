209d65ae9ecd9c95e9fce0cb39b488ea71acb95d
y-du <42994541+y-du@users.noreply.github.com>
2026-03-12 07:59:15 +0100

    add files merge and check functions, change how file group ids are generated

--- snrgy_3822/pkg/components/handler/deployments/files.go
+++ snrgy_3822/pkg/components/handler/deployments/files.go
@@ -22,8 +22,12 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
+	"maps"
+	"os"
+	"path"
 	"strings"
 
+	helper_naming "github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/naming"
 	models_external "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/external"
 	models_handler_deployment "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/deployment"
 	models_handler_storage "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/storage"
@@ -59,21 +63,18 @@ func fileToBytes(fSys fs.FS, path string) ([]byte, error) {
 
 func getProvidedFiles(
 	moduleFiles map[string]models_external.ModuleFile,
-	defaultFiles map[string][]byte, userInputs map[string][]byte,
+	defaultFiles map[string][]byte,
+	userInputs map[string][]byte,
 	deploymentId string,
-) (map[string]models_handler_storage.DeploymentFile, error) {
+) map[string]models_handler_storage.DeploymentFile {
 	files := make(map[string]models_handler_storage.DeploymentFile)
-	var errs []string
-	for reference, file := range moduleFiles {
-		defaultData, defaultOK := defaultFiles[reference]
+	for reference := range moduleFiles {
 		data, ok := userInputs[reference]
-		if !ok {
-			if file.Required && !defaultOK {
-				errs = append(errs, fmt.Sprintf("missing required file '%s'", reference))
-			}
+		if !ok || len(data) == 0 {
 			continue
 		}
-		if defaultOK && bytes.Equal(data, defaultData) {
+		defaultData, ok := defaultFiles[reference]
+		if ok && bytes.Equal(data, defaultData) {
 			continue
 		}
 		files[reference] = models_handler_storage.DeploymentFile{
@@ -82,10 +83,36 @@ func getProvidedFiles(
 			Data:         data,
 		}
 	}
-	if len(errs) > 0 {
-		return nil, errors.New(strings.Join(errs, "\n")) // TODO
+	return files
+}
+
+func mergeFiles(
+	defaultFiles map[string][]byte,
+	providedFiles map[string]models_handler_storage.DeploymentFile,
+) map[string][]byte {
+	files := make(map[string][]byte)
+	maps.Copy(files, defaultFiles)
+	for reference, file := range providedFiles {
+		files[reference] = file.Data
 	}
-	return files, nil
+	return files
+}
+
+func checkFiles(
+	moduleFiles map[string]models_external.ModuleFile,
+	files map[string][]byte,
+) error {
+	var errs []string
+	for reference, moduleFile := range moduleFiles {
+		_, ok := files[reference]
+		if !ok && moduleFile.Required {
+			errs = append(errs, fmt.Sprintf("file %s required", reference))
+		}
+	}
+	if len(errs) > 0 {
+		return errors.New(strings.Join(errs, "\n"))
+	}
+	return nil
 }
 
 func getProvidedFileGroups(
@@ -99,6 +126,7 @@ func getProvidedFileGroups(
 		if !ok {
 			continue
 		}
+		id := helper_naming.GenHash(deploymentId, reference)
 		var files []models_handler_storage.DeploymentFileGroupFile
 		for path, input := range fg {
 			files = append(files, models_handler_storage.DeploymentFileGroupFile{
@@ -108,7 +136,7 @@ func getProvidedFileGroups(
 			})
 		}
 		fileGroups[reference] = models_handler_storage.DeploymentFileGroup{
-			Id:           deploymentId + "_" + reference,
+			Id:           id,
 			DeploymentId: deploymentId,
 			Reference:    reference,
 			Files:        files,
