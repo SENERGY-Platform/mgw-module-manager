8b2fe955111d23dad96d6302fe37ca022f197023
y-du <42994541+y-du@users.noreply.github.com>
2026-03-24 09:43:49 +0100

    init delete deployments

---
+++ pkg/components/handler/deployments/delete.go
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2026 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package deployments
+
+import (
+	"context"
+	"errors"
+	"maps"
+	"slices"
+	"strings"
+
+	models_handler_deployment "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/deployment"
+	models_handler_storage "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/storage"
+)
+
+func (h *Handler) DeleteDeployments(ctx context.Context, filter models_handler_deployment.DeploymentsFilter) error {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+	deployments, err := h.storageHdl.ReadDeployments(ctx, filter.DeploymentsFilter)
+	if err != nil {
+		return err
+	}
+	deploymentsVolumes, deploymentsContainers, err := h.getDeploymentsVolumesAndContainersFromDB(ctx, slices.Collect(maps.Keys(deployments)))
+	if err != nil {
+		return err
+	}
+	var errs []string
+	for id, deployment := range deployments {
+		err = h.deleteDeployment(
+			ctx,
+			deployment.Id,
+			deployment.DirName,
+			deployment.FilesDirName,
+			deploymentsContainers[id],
+			deploymentsVolumes[id],
+		)
+		if err != nil {
+			errs = append(errs, err.Error())
+		}
+	}
+	if len(errs) > 0 {
+		return errors.New(strings.Join(errs, "\n"))
+	}
+	return nil
+}
+
+func (h *Handler) deleteDeployment(
+	ctx context.Context,
+	deploymentId string,
+	deploymentDirName string,
+	deploymentFilesDirName string,
+	containers map[string]models_handler_storage.DeploymentContainer,
+	volumes map[string]models_handler_storage.DeploymentVolume,
+) error {
+	err := h.removeDeploymentEnvironment(ctx, deploymentId, deploymentDirName, deploymentFilesDirName, containers)
+	if err != nil {
+		return err
+	}
+	err = h.removeContainerVolumes(ctx, volumes)
+	if err != nil {
+		return err
+	}
+	return h.storageHdl.DeleteDeployment(ctx, deploymentId)
+}
