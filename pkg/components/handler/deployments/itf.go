813a606cecb54365fe4ee467ed315b3b27f57fce
y-du <42994541+y-du@users.noreply.github.com>
2026-03-24 10:41:07 +0100

    remove unused methods

--- pkg/components/handler/deployments/itf.go
+++ pkg/components/handler/deployments/itf.go
@@ -95,7 +95,6 @@ type storageHandler interface {
 
 type containerEngineWrapperClient interface {
 	GetContainers(ctx context.Context, filter models_external.ContainersFilter) ([]models_external.Container, error)
-	GetContainer(ctx context.Context, id string) (models_external.Container, error)
 	CreateContainer(ctx context.Context, container models_external.Container) (id string, err error)
 	StartContainer(ctx context.Context, id string) error
 	StopContainer(ctx context.Context, id string) (jobId string, err error)
@@ -103,9 +102,7 @@ type containerEngineWrapperClient interface {
 	RemoveContainer(ctx context.Context, id string, force bool) error
 	GetImage(ctx context.Context, id string) (models_external.Image, error)
 	AddImage(ctx context.Context, img string) (jobId string, err error)
-	GetImages(ctx context.Context, filter models_external.ImagesFilter) ([]models_external.Image, error)
 	GetVolumes(ctx context.Context, filter models_external.VolumesFilter) ([]models_external.Volume, error)
-	GetVolume(ctx context.Context, id string) (models_external.Volume, error)
 	CreateVolume(ctx context.Context, vol models_external.Volume) (string, error)
 	RemoveVolume(ctx context.Context, id string, force bool) error
 	GetJob(ctx context.Context, id string) (models_external.Job, error)
