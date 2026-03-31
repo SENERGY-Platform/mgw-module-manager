dbcd67fc02232ba26974d4c0472fc1a79e9b3a50
y-du <42994541+y-du@users.noreply.github.com>
2026-03-31 08:44:30 +0200

    remove import

--- pkg/service/module.go
+++ pkg/service/module.go
@@ -23,7 +23,6 @@ import (
 	"reflect"
 	"slices"
 
-	helper_maps "github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/maps"
 	helper_time "github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/time"
 	models_error "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/error"
 	models_handler_deployment "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/deployment"
