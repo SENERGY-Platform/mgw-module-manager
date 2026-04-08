a278ed33be355cbc7e9bbcbc6b9f5fa4f704182a
y-du <42994541+y-du@users.noreply.github.com>
2026-04-08 14:49:55 +0200

    init

---
+++ pkg/service/job.go
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2026 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package service
+
+import (
+	"context"
+	"errors"
+	"slices"
+
+	handler_jobs "github.com/SENERGY-Platform/mgw-module-manager/pkg/components/handler/jobs"
+	models_service "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/service"
+)
+
+func (s *Service) Jobs(_ context.Context, filterIds []string) ([]models_service.Job, error) {
+	handlerJobs := s.jobsHandler.Jobs(filterIds)
+	var jobs []models_service.Job
+	for _, handlerJob := range handlerJobs {
+		jobs = append(jobs, getJob(handlerJob))
+	}
+	slices.SortStableFunc(jobs, func(a, b models_service.Job) int {
+		return a.Start.Compare(b.Start)
+	})
+	return jobs, nil
+}
+
+func (s *Service) Job(_ context.Context, jobId string) (models_service.Job, error) {
+	handlerJob, ok := s.jobsHandler.Job(jobId)
+	if !ok {
+		return models_service.Job{}, errors.New("not found") // TODO
+	}
+	return getJob(handlerJob), nil
+}
+
+func (s *Service) CancelJobs(_ context.Context, jobIds []string) error {
+	handlerJobs := s.jobsHandler.Jobs(jobIds)
+	for _, handlerJob := range handlerJobs {
+		handlerJob.Cancel()
+	}
+	return nil
+}
+
+func (s *Service) CancelJob(_ context.Context, jobId string) error {
+	handlerJob, ok := s.jobsHandler.Job(jobId)
+	if !ok {
+		return errors.New("not found") // TODO
+	}
+	handlerJob.Cancel()
+	return nil
+}
+
+func getJob(handlerJob *handler_jobs.Job) models_service.Job {
+	runtimeData := handlerJob.RuntimeData()
+	job := models_service.Job{
+		Id:          handlerJob.Id,
+		Description: handlerJob.Description,
+		Start:       handlerJob.Start,
+		End:         runtimeData.End,
+	}
+	if runtimeData.Error != nil {
+		job.ErrorMessage = runtimeData.Error.Error()
+		job.HasError = true
+	}
+	return job
+}
