65270d9fe7d17ce3c2e391d0ee6b2cdcfdd4bb4b
y-du <42994541+y-du@users.noreply.github.com>
2026-04-17 11:42:28 +0200

    init

---
+++ pkg/components/handler/aux_deployments/logger.go
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2026 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package handler_aux_deployments
+
+import (
+	"log/slog"
+
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/slog_attr"
+)
+
+var logger *slog.Logger
+
+func InitLogger(sl *slog.Logger) {
+	logger = sl.With(slog_attr.ComponentKey, "handler-auxiliary-deployments")
+}
