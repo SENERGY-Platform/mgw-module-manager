b9e963bb1318df3143fe51ee1a362a51355b1534
y-du <42994541+y-du@users.noreply.github.com>
2026-04-17 14:41:39 +0200

    init

---
+++ pkg/components/handler/aux_deployments/delete.go
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2026 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package handler_aux_deployments
+
+import (
+	"context"
+	"errors"
+	"strings"
+
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/containers"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/aux_deployments"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/database"
+)
+
+func (h *Handler) DeleteAuxiliaryDeployments(
+	ctx context.Context,
+	deploymentId string,
+	filter models_handler_aux_deployments.AuxiliaryDeploymentsFilter,
+) error {
+	auxDeployments, err := h.databaseHandler.ReadAuxiliaryDeployments(ctx, deploymentId, filter.AuxiliaryDeploymentsFilter)
+	if err != nil {
+		return err
+	}
+	if filter.State != "" {
+		cewContainers, err := h.getCewContainers(ctx, auxDeployments)
+		if err != nil {
+			return err
+		}
+		tmp := make(map[string]models_handler_database.AuxiliaryDeployment)
+		for id, auxDep := range auxDeployments {
+			cewContainer := cewContainers[auxDep.Container.Name]
+			if cewContainer.State == filter.State {
+				tmp[id] = auxDep
+			}
+		}
+		auxDeployments = tmp
+	}
+	var errs []string
+	for id, auxDep := range auxDeployments {
+		err = h.deleteAuxiliaryDeployment(ctx, id, auxDep.Container.Name)
+		if err != nil {
+			errs = append(errs, err.Error())
+		}
+	}
+	if len(errs) > 0 {
+		return errors.New(strings.Join(errs, "\n")) // TODO
+	}
+	return nil
+}
+
+func (h *Handler) deleteAuxiliaryDeployment(ctx context.Context, id, containerName string) error {
+	err := helper_containers.Remove(ctx, h.containerEngineWrapperClient, containerName)
+	if err != nil {
+		return err
+	}
+	err = h.databaseHandler.DeleteAuxiliaryDeployment(ctx, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
