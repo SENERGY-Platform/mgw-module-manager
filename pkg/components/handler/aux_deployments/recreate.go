5a5d6ab8cefe47d343d9a88941a77be9a9216aac
y-du <42994541+y-du@users.noreply.github.com>
2026-04-20 14:10:55 +0200

    init

---
+++ pkg/components/handler/aux_deployments/recreate.go
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2026 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package handler_aux_deployments
+
+import (
+	"context"
+	"errors"
+	"maps"
+	"slices"
+	"strings"
+
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/containers"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/external"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/aux_deployments"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/database"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/deployments"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/modules"
+)
+
+func (h *Handler) RecreateAuxiliaryDeployments(
+	ctx context.Context,
+	module models_handler_modules.Module,
+	activeDeployment models_handler_deployments.Deployment,
+	dependencies map[string]models_handler_deployments.DeploymentReduced,
+	filter models_handler_aux_deployments.AuxiliaryDeploymentsFilter,
+) ([]string, error) {
+	mu := h.mutexes.Get(activeDeployment.Id)
+	mu.RLock()
+	defer mu.RUnlock()
+	auxDeployments, err := h.databaseHandler.ReadAuxiliaryDeployments(ctx, activeDeployment.Id, filter.AuxiliaryDeploymentsFilter)
+	if err != nil {
+		return nil, err
+	}
+	if filter.State != "" {
+		cewContainers, err := h.getCewContainers(ctx, auxDeployments)
+		if err != nil {
+			return nil, err
+		}
+		tmp := make(map[string]models_handler_database.AuxiliaryDeployment)
+		for id, auxDep := range auxDeployments {
+			cewContainer := cewContainers[auxDep.Container.Name]
+			if cewContainer.State == filter.State {
+				tmp[id] = auxDep
+			}
+		}
+		auxDeployments = tmp
+	}
+	auxDepIds := slices.Collect(maps.Keys(auxDeployments))
+	auxDepConfigs, err := h.databaseHandler.ReadAuxiliaryDeploymentsConfigs(ctx, auxDepIds)
+	if err != nil {
+		return nil, err
+	}
+	auxDepVolumeMounts, err := h.databaseHandler.ReadAuxiliaryDeploymentsVolumeMounts(ctx, auxDepIds)
+	if err != nil {
+		return nil, err
+	}
+	auxDeploymentVolumes, err := h.databaseHandler.ReadAuxiliaryDeploymentVolumes(ctx, activeDeployment.Id)
+	if err != nil {
+		return nil, err
+	}
+	err = h.ensureContainerVolumes(ctx, auxDeploymentVolumes, activeDeployment.Id)
+	if err != nil {
+		return nil, err
+	}
+	cacheDeploymentConfigs := make(map[string]map[string]string)
+	var recreated []string
+	var errs []string
+	for id, auxDep := range auxDeployments {
+		auxService, ok := module.AuxServices[auxDep.Reference]
+		if !ok {
+			errs = append(errs, "auxiliary service reference not found") // TODO
+			continue
+		}
+		deploymentConfigs, ok := cacheDeploymentConfigs[auxDep.Reference]
+		if !ok {
+			deploymentConfigs, err = getDeploymentConfigs(module.Configs, auxService.Configs, activeDeployment.Configs)
+			if err != nil {
+				errs = append(errs, err.Error())
+				continue
+			}
+		}
+		err = h.recreateAuxiliaryDeployment(
+			ctx,
+			auxService,
+			activeDeployment,
+			dependencies,
+			deploymentConfigs,
+			auxDep,
+			auxDepConfigs[id],
+			auxDepVolumeMounts[id],
+		)
+		if err != nil {
+			errs = append(errs, err.Error())
+			continue
+		}
+		recreated = append(recreated, id)
+	}
+	if len(errs) > 0 {
+		return recreated, errors.New(strings.Join(errs, "\n")) // TODO
+	}
+	return recreated, nil
+}
+
+func (h *Handler) recreateAuxiliaryDeployment(
+	ctx context.Context,
+	auxService models_external.ModuleLibAuxService,
+	activeDeployment models_handler_deployments.Deployment,
+	dependencies map[string]models_handler_deployments.DeploymentReduced,
+	deploymentConfigs map[string]string,
+	currentAuxDeployment models_handler_database.AuxiliaryDeployment,
+	configs map[string]string,
+	volumeMounts []models_handler_database.AuxiliaryDeploymentVolumeMount,
+) error {
+	err := helper_containers.Stop(ctx, h.containerEngineWrapperClient, currentAuxDeployment.Container.Name, h.config.JobPollInterval)
+	if err != nil {
+		return err
+	}
+	err = helper_containers.Remove(ctx, h.containerEngineWrapperClient, currentAuxDeployment.Container.Name)
+	if err != nil {
+		return err
+	}
+	err = helper_containers.EnsureImage(
+		ctx,
+		h.containerEngineWrapperClient,
+		currentAuxDeployment.Image,
+		false,
+		h.config.PathEscapeDepth,
+		h.config.JobPollInterval,
+	)
+	if err != nil {
+		return err
+	}
+	return h.createContainer(
+		ctx,
+		auxService,
+		currentAuxDeployment.Reference,
+		activeDeployment,
+		dependencies,
+		currentAuxDeployment,
+		mergeConfigs(deploymentConfigs, configs),
+		volumeMounts,
+	)
+}
