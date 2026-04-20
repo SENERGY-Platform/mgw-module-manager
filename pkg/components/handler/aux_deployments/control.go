329fae8abd9786041a3b585f6f810cc61b5e5205
y-du <42994541+y-du@users.noreply.github.com>
2026-04-20 17:09:49 +0200

    init

---
+++ pkg/components/handler/aux_deployments/control.go
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2026 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package handler_aux_deployments
+
+import (
+	"context"
+	"maps"
+	"slices"
+
+	models_handler_aux_deployments "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/aux_deployments"
+)
+
+func (h *Handler) EnableAuxiliaryDeployments(
+	ctx context.Context,
+	deploymentId string,
+	filter models_handler_aux_deployments.AuxiliaryDeploymentsFilter,
+) error {
+	mu := h.mutexes.Get(deploymentId)
+	mu.RLock()
+	defer mu.RUnlock()
+	auxDeployments, err := h.readAuxiliaryDeploymentsAndFilterByState(ctx, deploymentId, filter)
+	if err != nil {
+		return err
+	}
+	return h.databaseHandler.UpdateAuxiliaryDeploymentsEnabledState(ctx, slices.Collect(maps.Keys(auxDeployments)), true)
+}
+
+func (h *Handler) DisableAuxiliaryDeployments(
+	ctx context.Context,
+	deploymentId string,
+	filter models_handler_aux_deployments.AuxiliaryDeploymentsFilter,
+) error {
+	mu := h.mutexes.Get(deploymentId)
+	mu.RLock()
+	defer mu.RUnlock()
+	auxDeployments, err := h.readAuxiliaryDeploymentsAndFilterByState(ctx, deploymentId, filter)
+	if err != nil {
+		return err
+	}
+	return h.databaseHandler.UpdateAuxiliaryDeploymentsEnabledState(ctx, slices.Collect(maps.Keys(auxDeployments)), false)
+}
