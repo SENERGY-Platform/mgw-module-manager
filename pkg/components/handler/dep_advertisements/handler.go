51ddcee47892a30c944a13e02a9b3a798da4f22d
y-du <42994541+y-du@users.noreply.github.com>
2026-04-28 16:41:19 +0200

    init

---
+++ pkg/components/handler/dep_advertisements/handler.go
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2026 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dep_advertisements
+
+import (
+	"context"
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
+
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/time"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/uuid"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/error"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/database"
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/dep_advertisements"
+)
+
+type Handler struct {
+	databaseHandler databaseHandler
+}
+
+func New(databaseHandler databaseHandler) *Handler {
+	return &Handler{databaseHandler: databaseHandler}
+}
+
+func (h *Handler) GetAdvertisement(
+	ctx context.Context,
+	deploymentId string,
+	reference string,
+) (models_handler_database.DeploymentAdvertisement, error) {
+	return h.databaseHandler.ReadDeploymentAdvertisement(ctx, deploymentId, reference)
+}
+
+func (h *Handler) GetAdvertisementById(ctx context.Context, id string) (models_handler_database.DeploymentAdvertisement, error) {
+	advertisements, err := h.databaseHandler.ReadDeploymentAdvertisements(ctx, models_handler_database.DeploymentAdvertisementsFilter{
+		Ids: []string{id},
+	})
+	if err != nil {
+		return models_handler_database.DeploymentAdvertisement{}, err
+	}
+	if len(advertisements) == 0 {
+		return models_handler_database.DeploymentAdvertisement{}, models_error.NotFoundErr
+	}
+	return advertisements[id], nil
+}
+
+func (h *Handler) GetAdvertisements(
+	ctx context.Context,
+	filter models_handler_database.DeploymentAdvertisementsFilter,
+) (map[string]models_handler_database.DeploymentAdvertisement, error) {
+	return h.databaseHandler.ReadDeploymentAdvertisements(ctx, filter)
+}
+
+func (h *Handler) PutAdvertisement(
+	ctx context.Context,
+	moduleId string,
+	deploymentId string,
+	reference string,
+	items map[string]string,
+) (string, error) {
+	advertisement, err := newDatabaseAdvertisement(moduleId, deploymentId, helper_time.Now(), reference, items)
+	if err != nil {
+		return "", err
+	}
+	err = h.databaseHandler.WriteDeploymentAdvertisements(
+		ctx,
+		deploymentId,
+		[]models_handler_database.DeploymentAdvertisement{advertisement},
+		true,
+	)
+	if err != nil {
+		return "", err
+	}
+	return advertisement.Id, nil
+}
+
+func (h *Handler) PutAdvertisements(
+	ctx context.Context,
+	moduleId string,
+	deploymentId string,
+	inputs []models_handler_dep_advertisements.DeploymentAdvertisementInput,
+	incremental bool,
+) ([]string, error) {
+	timestamp := helper_time.Now()
+	var advertisements []models_handler_database.DeploymentAdvertisement
+	var ids []string
+	for _, input := range inputs {
+		advertisement, err := newDatabaseAdvertisement(moduleId, deploymentId, timestamp, input.Reference, input.Items)
+		if err != nil {
+			return nil, err
+		}
+		advertisements = append(advertisements, advertisement)
+		ids = append(ids, advertisement.Id)
+	}
+	err := h.databaseHandler.WriteDeploymentAdvertisements(ctx, deploymentId, advertisements, incremental)
+	if err != nil {
+		return nil, err
+	}
+	return ids, nil
+}
+
+func (h *Handler) DeleteAdvertisements(ctx context.Context, deploymentId string, references []string, allowAll bool) error {
+	if !allowAll && len(references) == 0 {
+		return nil
+	}
+	return h.databaseHandler.DeleteDeploymentAdvertisements(ctx, deploymentId, references)
+}
+
+func newDatabaseAdvertisement(
+	moduleId string,
+	deploymentId string,
+	timestamp time.Time,
+	reference string,
+	items map[string]string,
+) (models_handler_database.DeploymentAdvertisement, error) {
+	id, err := helper_uuid.New()
+	if err != nil {
+		return models_handler_database.DeploymentAdvertisement{}, err
+	}
+	originHash := sha256.New()
+	originHash.Write([]byte(deploymentId))
+	return models_handler_database.DeploymentAdvertisement{
+		Id:        id,
+		ModuleId:  moduleId,
+		Origin:    hex.EncodeToString(originHash.Sum(nil)),
+		Reference: reference,
+		Timestamp: timestamp,
+		Items:     items,
+	}, nil
+}
---
+++ pkg/components/handler/dep_advertisements/itf.go
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2026 InfAI (CC SES)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dep_advertisements
+
+import (
+	"context"
+
+	"github.com/SENERGY-Platform/mgw-module-manager/pkg/models/handler/database"
+)
+
+type databaseHandler interface {
+	ReadDeploymentAdvertisement(
+		ctx context.Context,
+		deploymentId string,
+		reference string,
+	) (models_handler_database.DeploymentAdvertisement, error)
+	ReadDeploymentAdvertisements(
+		ctx context.Context,
+		filter models_handler_database.DeploymentAdvertisementsFilter,
+	) (map[string]models_handler_database.DeploymentAdvertisement, error)
+	WriteDeploymentAdvertisements(
+		ctx context.Context,
+		deploymentId string,
+		advertisements []models_handler_database.DeploymentAdvertisement,
+		incremental bool,
+	) error
+	DeleteDeploymentAdvertisements(ctx context.Context, deploymentId string, references []string) error
+}
