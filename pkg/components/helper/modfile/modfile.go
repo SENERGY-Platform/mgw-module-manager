2a3ee1ed466c1d7c984f1120a5efd7ae65b0ec2f
y-du <42994541+y-du@users.noreply.github.com>
2026-05-05 14:22:31 +0200

    change

--- pkg/components/helper/modfile/modfile.go
+++ pkg/components/helper/modfile/modfile.go
@@ -6,23 +6,23 @@ import (
 	"regexp"
 
 	"github.com/SENERGY-Platform/mgw-modfile-lib"
-	module_lib "github.com/SENERGY-Platform/mgw-module-lib/model"
-	"github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/file_sys"
+	helper_file_sys "github.com/SENERGY-Platform/mgw-module-manager/pkg/components/helper/file_sys"
+	models_external "github.com/SENERGY-Platform/mgw-module-manager/pkg/models/external"
 )
 
 var regExp = regexp.MustCompile(`^Modfile\.(?:yml|yaml)$`)
 
-func GetModule(fSys fs.FS) (module_lib.Module, error) {
+func GetModule(fSys fs.FS) (models_external.ModuleLibModule, error) {
 	mfPath, err := helper_file_sys.FindFile(fSys, regExp.MatchString)
 	if err != nil {
-		return module_lib.Module{}, err
+		return models_external.ModuleLibModule{}, err
 	}
 	if mfPath == "" {
-		return module_lib.Module{}, errors.New("modfile not found")
+		return models_external.ModuleLibModule{}, errors.New("modfile not found")
 	}
 	file, err := fSys.Open(mfPath)
 	if err != nil {
-		return module_lib.Module{}, err
+		return models_external.ModuleLibModule{}, err
 	}
 	defer file.Close()
 	return modfile_lib.Decode(file)
