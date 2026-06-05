1b6876cb81d94391484e172e842906ea023f0bff
y-du <42994541+y-du@users.noreply.github.com>
2026-06-05 11:55:31 +0200

    fix empty change request

--- pkg/service/module.go
+++ pkg/service/module.go
@@ -205,7 +205,9 @@ func (s *Service) CreateModulesUpdateAllChangeRequest(ctx context.Context) (lib_
 	if err != nil {
 		return lib_models.ModulesChangeRequest{}, err
 	}
-	s.changeRequest = &changeRequest
+	if len(changeRequest.Install) > 0 || len(changeRequest.Change) > 0 || len(changeRequest.Remove) > 0 {
+		s.changeRequest = &changeRequest
+	}
 	return transformModulesChangeRequest(changeRequest), nil
 }
 
