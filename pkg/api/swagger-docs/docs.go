b43984563ce4c79a65383efdb0495e8d88a86618
y-du <42994541+y-du@users.noreply.github.com>
2026-08-13 10:28:48 +0200

    update swagger docs

--- pkg/api/swagger-docs/docs.go
+++ pkg/api/swagger-docs/docs.go
@@ -4424,7 +4424,7 @@ const docTemplate = `{
         },
         "/swagger": {
             "get": {
-                "description": "Redirect to the swagger UI entrypoint. Required because the swagger handler only serves named resources and the engine does not append trailing slashes.",
+                "description": "Redirect to the swagger UI entrypoint.",
                 "tags": [
                     "swagger"
                 ],
@@ -6572,10 +6572,18 @@ const docTemplate = `{
                 1000,
                 1000000,
                 1000000000,
+                1,
+                1000,
+                1000000,
+                1000000000,
                 60000000000,
                 3600000000000
             ],
             "x-enum-varnames": [
+                "Nanosecond",
+                "Microsecond",
+                "Millisecond",
+                "Second",
                 "Nanosecond",
                 "Microsecond",
                 "Millisecond",
